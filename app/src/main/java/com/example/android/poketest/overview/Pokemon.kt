81e26dfb22bf5c19c08205840d87e042b546c3b3
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-06-23 19:51:22 -0400

    Added Fragment under Main Activity

---
+++ app/src/main/java/com/example/android/poketest/overview/Pokemon.kt
@@ -0,0 +1,33 @@
+package com.example.android.poketest.overview
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.poketest.R
+
+class Pokemon : Fragment() {
+
+    companion object {
+        fun newInstance() = Pokemon()
+    }
+
+    private lateinit var viewModel: PokemonViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.pokemon_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(PokemonViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/android/poketest/overview/PokemonViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.android.poketest.overview
+
+import androidx.lifecycle.ViewModel
+
+class PokemonViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
